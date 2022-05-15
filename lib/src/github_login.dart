abfe69ffccd4f285c84a911d6f755c725ecf2ef0
arpangreat <arpanthegreat41@gmail.com>
2022-05-16 00:27:48 +0530

    clean ups

---
+++ lib/src/github_login.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class GithubLoginWidget extends StatefulWidget {
+  const GithubLoginWidget({
+    Key? key,
+    required this.builder,
+  }) : super(key: key);
+
+  final WidgetBuilder builder;
+
+  @override
+  State<GithubLoginWidget> createState() => _GithubLoginWidgetState();
+}
+
+class _GithubLoginWidgetState extends State<GithubLoginWidget> {
+  bool _isLoggedIn = false;
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoggedIn) {
+      return widget.builder(context);
+    }
+
+    return ElevatedButton(
+        onPressed: () {
+          setState(() {
+            _isLoggedIn = true;
+          });
+        },
+        child: const Text('Log in to fake Github'));
+  }
+}
